bf31acf3b886e2a37eb41608f9d20f4d20f2516d
Eduardo Picazo <edy7picazo@gmail.com>
2024-01-30 15:47:37 -0600

    fix: remove unused import

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { trpc } from '../app/_trpc/client';
 import { httpBatchLink } from '@trpc/client';
-import { absoluteUrl, apiUrl } from '../lib/utils';
+import { absoluteUrl } from '../lib/utils';
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
